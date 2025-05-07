8b59edfb1406da5e1f999f2d2438a3393e29414d
Nikilitha <kakazanikilitha544@gmail.com>
2025-05-07 16:06:53 +0200

    about responsiveness

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <nav> 
+    <nav>
       <!-- Navigation content if any -->
     </nav>
 
@@ -30,9 +30,9 @@
         <div class="team-member">
           <img src="https://nikilitha26.github.io/Boot-img/Image/IMG_8063.PNG" alt="Team Member 1">
           <div class="team-info">
-            <h3>Nikilitha Kakaza</h3>
-            <p>Founder & CEO</p>
-            <p>Nikilitha is the CEO and web developer of Majestic Scent. With years of experience in web development, Nikilitha has established a reputation for creating visually appealing, user-friendly websites that effectively convey the essence of a brand.
+            <h3 class="h3">Nikilitha Kakaza</h3>
+            <p class="founder">Founder & CEO</p>
+            <p class="infoo">Nikilitha is the CEO and web developer of Majestic Scent. With years of experience in web development, Nikilitha has established a reputation for creating visually appealing, user-friendly websites that effectively convey the essence of a brand.
               As a CEO, Nikilitha is deeply committed to ensuring that every aspect of the business meets the highest standards of excellence. This dedication extends to the company's online presence, where Nikilitha works tirelessly to ensure that the website accurately reflects the values and vision of Majestic Scent.</p>
           </div>
         </div>
@@ -40,9 +40,9 @@
         <div class="team-member">
           <img src="https://danielumetor.github.io/allimages/20240422_092918.jpg" alt="Team Member 2">
           <div class="team-info">
-            <h3>Daniel Umetor</h3>
-            <p>Co-Founder & CMO</p>
-            <p>Meet Daniel Umetor, the co-founder and CEO of Majestic Scent. With a background in finance and a passion for fragrance, Daniel brings a unique blend of skills to the table. As a CEO, Daniel is responsible for overseeing the overall direction of the company, ensuring that it stays true to its core values while striving for growth and success.
+            <h3 class="h3">Daniel Umetor</h3>
+            <p class="founder">Co-Founder & CMO</p>
+            <p class="infoo">Meet Daniel Umetor, the co-founder and CEO of Majestic Scent. With a background in finance and a passion for fragrance, Daniel brings a unique blend of skills to the table. As a CEO, Daniel is responsible for overseeing the overall direction of the company, ensuring that it stays true to its core values while striving for growth and success.
               Daniel is a firm believer in the power of fragrance to bring joy and positivity to people's lives.</p>
           </div>
         </div>
@@ -51,7 +51,7 @@
   </div>
 </template>
 
-<style>
+<style scoped>
 .about {
   background: black;
   min-height: 100vh;
@@ -69,7 +69,6 @@
   position: relative;
   bottom: 50px;
 }
-
 
 .about-us {
   color: #ad8330;
@@ -155,7 +154,6 @@
   }
 }
 
-
 .our-team-title {
   font-size: 36px;
   font-weight: bold;
@@ -163,6 +161,7 @@
   text-align: center;
   margin-bottom: 20px;
   position: relative;
+  font-family: "Cinzel", serif;
 }
 
 .our-team-title::after {
@@ -182,6 +181,7 @@
   flex-wrap: wrap;
   justify-content: center;
   margin-top: 40px;
+  font-family: "Cinzel", serif;
 }
 
 .team-member {
@@ -189,25 +189,19 @@
   flex-direction: row;
   align-items: center;
   margin: 20px;
-  background-color: #000; 
-  color: #ad8330;   
+  background-color: #000;
+  color: #ad8330;
   padding: 20px;
-  border: 2px solid #ad8330; 
+  border: 2px solid #ad8330;
   border-radius: 10px;
-  box-shadow: 0 0 10px rgba(173, 131, 48, 0.5); 
+  box-shadow: 0 0 10px rgba(173, 131, 48, 0.5);
+  font-family: "Cinzel", serif;
 }
 
 .team-member:hover {
-  background-color: #1a1a1a;           
+  background-color: #1a1a1a;
   box-shadow: 0 0 15px rgba(173, 131, 48, 0.8);
 }
-
-/* .team-member {
-  background-color: #000;             
-  border: 2px solid #ad8330;           
-  color: #ad8330;                     
-  box-shadow: 0 0 10px rgba(173, 131, 48, 0.5); 
-} */
 
 .team-member img {
   width: 100px;
@@ -221,46 +215,185 @@
   flex: 1;
   color: #ad8330;
   text-align: justify;
+  font-family: "Cinzel", serif;
 }
 
 .team-info h3 {
   margin-top: 0;
   color: #ad8330;
+  font-family: "Cinzel", serif;
 }
 
-@media only screen and (max-width: 300px) {
+@media (max-width: 768px) {
   .about {
-    padding-top: 20px;
+    padding-top: 160px;
+    padding-bottom: 20px; 
   }
+
   .about-container {
     max-width: 90%;
     padding: 10px;
   }
+
+  .about-us {
+    font-size: 48px !important;
+  }
+
   .about-paragraph {
-    font-size: 14px;
+    position: relative;
+    bottom: 20px !important;
+    font-size: 35px;
     padding: 10px;
   }
+
+  /* bottom-right */
+.sparkle-br {
+  bottom: 20px;
+  right: -10px;
+  animation-delay: 1.5s;
+}
+
   .our-team {
     flex-direction: column;
     align-items: center;
   }
+
+  .h3{
+    position: relative;
+    top: 10px;
+    font-size: 42px !important;
+    font-weight: 900;
+  }
+
+  .founder{
+    font-size: 38px !important;
+    font-weight: 550;
+  }
+  .infoo{
+    font-size: 30px !important;
+    text-align: justify;
+  }
+
+  .our-team-title{
+    font-size: 48px !important;
+  }
+
   .team-member {
     flex-direction: column;
     align-items: center;
     margin: 10px;
     padding: 10px;
   }
-  
+
   .team-member img {
-    width: 50px;
-    height: 50px;
+    width: 200px !important;
+    height: 200px !important;
   }
+
   .team-info {
-    text-align: center;
+    text-align: justify;
   }
+
   .our-team-title {
     font-size: 24px;
     margin-bottom: 10px;
   }
 }
+
+@media (max-width: 480px) {
+  .about {
+    margin-top: 40px !important;
+    padding-bottom: 20px; 
+  }
+
+  .about-container {
+    max-width: 90%;
+    padding: 10px;
+  }
+
+  .about-us {
+    font-size: 22px;
+    margin-bottom: 5px;
+  }
+
+  .about-paragraph {
+    font-size: 20px;
+    padding: 12px;
+  }
+
+  /* top-left */
+.sparkle-tl {
+  top: 30px;
+  left: -30px;
+}
+
+/* top-right */
+.sparkle-tr {
+  top: 30px;
+  right: -20px;
+  animation-delay: 0.5s;
+}
+
+/* bottom-left */
+.sparkle-bl {
+  bottom: 20px;
+  left: -30px;
+  animation-delay: 1s;
+}
+
+/* bottom-right */
+.sparkle-br {
+  bottom: 20px;
+  right: -20px;
+  animation-delay: 1.5s;
+}
+
+  .our-team {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .our-team-title{
+    font-size: 28px !important;
+  }
+
+  .team-member {
+    flex-direction: column;
+    align-items: center;
+    padding: 8px;
+    margin: 8px;
+  }
+
+  .h3{
+    position: relative;
+    top: 10px;
+    font-size: 26px !important;
+    font-weight: 900;
+  }
+
+  .founder{
+    font-size: 26px !important;
+    font-weight: 550;
+  }
+  .infoo{
+    font-size: 20px !important;
+    text-align: justify;
+  }
+
+  .team-member img {
+    width: 40px;
+    height: 40px;
+  }
+
+  .team-info {
+    text-align: center;
+    padding: 0 10px;
+  }
+
+  .our-team-title {
+    font-size: 24px;
+    margin-bottom: 10px;
+  }
+}
+
 </style>
